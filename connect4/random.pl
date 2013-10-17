d2d9bb798a41d63b82b079fb5da984ac89fc553f
Dr3yla <dr3yla@gmail.com>
2013-10-17 12:15:52 +0200

    Update random.pl

--- connect4/random.pl
+++ connect4/random.pl
@@ -1,4 +1,4 @@
-%Always returns true
+%Have to always returns true -> vérifier si le coup est jouable sinon problème
 %This rules determins what move the ia will do.
-%Issue when the move can be done
+
 ia(X, Y) :- X is random(6), Y is random(5), add(X, Y, jaune), ! ; ia(X, Y).

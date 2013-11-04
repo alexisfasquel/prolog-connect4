4e31d97e7c220eeb88c69183cade79302c819c2f
VONCK <eleonore.vonck@insa-lyon.fr>
2013-11-04 17:15:15 +0100

    Update test_eval.pl

--- connect4/test_eval.pl
+++ connect4/test_eval.pl
@@ -1,9 +1,18 @@
-test(Nombre,V1,V2):- Nombre == 0.
-test(Nombre,V1,V2):- testv1(1,1,rouge),Nombre1 is Nombre-1,test(Nombre1,V1,V2).
+
+victoire(CouleurVictoire,VJ,VR):- CouleurVictoire == jaune,VJ is 1,VR is 0.
+victoire(CouleurVictoire,VJ,VR):- CouleurVictoire == rouge,VJ is 0,VR is 1.
+victoire(CouleurVictoire,VJ,VR):- CouleurVictoire == none,VJ is 0,VR is 0.
+
+test(Nombre,V1,V2,V3,V4):- Nombre == 0,write('jaune:'),write(V1),write(' rouge :'),write(V2).
+test(Nombre,V1,V2,V3,V4):- testv1(1,1,rouge,CouleurVictoire),victoire(CouleurVictoire,V3,V4),V11 is V1+V3,V22 is V2+V4,Nombre1 is Nombre-1,test(Nombre1,V11,V22,V33,V44).
+
+matchnull:- isfull(1),isfull(2),isfull(3),isfull(4),isfull(5),isfull(6),isfull(7).
+
 
 %Fonction qui fait jouer une ia contre une autre jusqu'a ce qu une des deux ia gagne
-%Voir encas de match nul
 
-testv1(X,Y,Color):- (win(X, Y, rouge),pawn(X, Y, rouge),write('rouge'),display,clear); (win(X, Y, jaune),pawn(X, Y, jaune),write('jaune'),display,clear).
-testv1(X,Y,Color):- Color == rouge,iarandom(X1,Y1),testv1(X1,Y1,jaune).
-testv1(X,Y,Color):- Color == jaune,play2(X1,Y1),testv1(X1,Y1,rouge).
+testv1(X,Y,Color,CouleurVictoire):- (win(X, Y, rouge),pawn(X, Y, rouge),display,clear,CouleurVictoire = rouge); (win(X, Y, jaune),pawn(X, Y, jaune),display,clear,CouleurVictoire = jaune);(matchnull,display,CouleurVictoire = none,write('null'),clear).
+testv1(X,Y,Color,CouleurVictoire):- Color == rouge,iarandom(X1,Y1),display,testv1(X1,Y1,jaune,CouleurVictoire).
+testv1(X,Y,Color,CouleurVictoire):- Color == jaune,play2(X1,Y1),display,testv1(X1,Y1,rouge,CouleurVictoire).
+
+fonctiontestqualite(NBtest):- integer(NBtest),test(NBtest,0,0,VR,VJ).
